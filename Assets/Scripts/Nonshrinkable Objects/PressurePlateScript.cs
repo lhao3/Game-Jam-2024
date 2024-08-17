cf08f813e46c581a92a17e564d55945e43b43808
Jiajun Liu <jliu618@berkeley.edu>
2024-08-17 18:45:41 -0400

    try again

--- Assets/Scripts/Nonshrinkable Objects/PressurePlateScript.cs
+++ Assets/Scripts/Nonshrinkable Objects/PressurePlateScript.cs
@@ -1,7 +1,27 @@
-﻿using System;
-public class NewClass
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlateScript : NonShrinkableScript
 {
-    public NewClass()
+
+    public bool activated;
+    DoorScript linkedDoor;
+
+    // Start is called before the first frame update
+    void Start()
     {
+        activated = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activated)
+        {
+            linkedDoor.opened = true;
+        }
+
     }
 }
