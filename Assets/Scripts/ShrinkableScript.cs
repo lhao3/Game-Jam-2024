ac7a827fcadd2d3931531e90b71116f6cf15e401
patrickzyin <patrickyin3@gmail.com>
2024-08-18 03:15:55 +0800

    shrinking works :)

--- Assets/Scripts/ShrinkableScript.cs
+++ Assets/Scripts/ShrinkableScript.cs
@@ -5,40 +5,58 @@ using UnityEngine;
 public class ShrinkableScript : MonoBehaviour
 {
     public string size;
-    public int xScale;
-    public int yScale;
+    public float xScale = 1f; // Default value, adjust as needed
+    public float yScale = 1f; // Default value, adjust as needed
+    const float maxSize = 2f; // Max size
+    const float minSize = 0.5f; // Min size
+    [SerializeField] private float scaleSpeed = 1f; // Speed of scaling
+
+    private Vector3 targetScale;
+    private bool scaling = false;
+    private float scaleFactor;
+
     // Start is called before the first frame update
     void Start()
     {
         size = "normal";
+        transform.localScale = new Vector3(xScale, yScale, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    void Shrink()
-    {
-        if (CheckSize())
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            size = "small";
+            if (size == "normal" || size == "large")
+            {
+                SetScaling(minSize, 0.5f); // Shrink to minSize
+                size = "small";
+            }
+            else if (size == "small")
+            {
+                SetScaling(maxSize, 1.5f); // Grow to maxSize
+                size = "large";
+            }
+        }
+
+        if (scaling)
+        {
+            // Gradually scale towards the target size
+            transform.localScale = Vector3.Lerp(transform.localScale, targetScale, scaleSpeed * Time.deltaTime);
+
+            // Stop scaling if close enough to the target scale
+            if (Vector3.Distance(transform.localScale, targetScale) < 0.01f)
+            {
+                transform.localScale = targetScale;
+                scaling = false;
+            }
         }
     }
 
-    void Grow()
+    private void SetScaling(float targetSize, float factor)
     {
-        if (CheckSize())
-        {
-            size = "large";
-        }
-    }
-
-    // TODO: come up with algorithm here
-    // if resulting dimensions overlap with other objects/environemnt, return false, else return true
-    private bool CheckSize()
-    {
-        return true;
+        targetScale = new Vector3(targetSize, targetSize, 1f);
+        scaling = true;
+        scaleFactor = factor;
     }
 }
