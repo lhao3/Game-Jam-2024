dc85df0e92e790114e1a754254e2ea7104513db8
lhao3 <linahao04@gmail.com>
2024-08-19 23:47:40 -0400

    Updated Pause Menu

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManagerScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public static GameManagerScript instance;
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private PlayerScript playerScript;
+    [SerializeField] private GameObject options;
+    private bool isMenuActive;
 
     private void Awake()
     {
@@ -29,6 +34,39 @@ public class GameManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isMenuActive = !isMenuActive;
+            pauseMenu.SetActive(isMenuActive);
+
+            if (isMenuActive)
+            {
+                Time.timeScale = 0f;
+                playerScript.DisablePlayerInput();
+            }
+            else
+            {
+                options.SetActive(false);
+                Time.timeScale = 1f;
+                playerScript.EnablePlayerInput();
+            }
+        }
     }
+
+    public void continuePressed()
+    {
+        Time.timeScale = 1f;
+        isMenuActive = false;
+        pauseMenu.SetActive(isMenuActive);
+        playerScript.EnablePlayerInput();
+
+    }
+
+    public void quitPressed()
+    {
+        playerScript.EnablePlayerInput();
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");  //Quit button in pause menu returns to main menu
+    }
+
 }
